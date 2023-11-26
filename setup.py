b76acd488edb42832e6ed14daa7f3c86cef7e050
Yuyao Huang <huangyuyao@outlook.com>
2023-11-26 19:26:26 +0800

    misc

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+import os
 from setuptools import setup
 
-with open("t3w/__init__.py", 'r') as script_file:
+
+with open(f"{os.path.dirname(__file__)}/t3w/__init__.py", 'r') as script_file:
     for line in script_file:
         if not line.startswith("__version__ = "): continue
         __version__ = eval(line[14:].rstrip()); break
